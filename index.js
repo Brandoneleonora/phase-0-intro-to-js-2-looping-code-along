78c2777cefb43fe1fcab848199a37b451bd9f7d5
Brandon Eleonora <brandoneleonora38@gmail.com>
2023-03-05 18:33:43 -0500

    complete project

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+function writeCards(names, thing){
+    const cards = [];
+    for(let i = 0; i < names.length; i++)
+    {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${thing} gift!`);
+    }       
+    console.log(cards);
+    return cards;
+}
+
+ writeCards([Guadalupe,Aki, Ollie], "birthday");
+ 
+
+ function countDown(i){
+    while (i >= 0){
+        console.log(i);
+        i--;
+     }
+ }
+ countDown(10);
